58e524678cd639a3feb39bfc5ba1f31adac4f9b8
Samuel <samueldeweerd@gmail.com>
2024-10-01 17:12:37 +0200

    added an observable that outputs a value for an interval of time. Also created an operator for the observable that transforms this value.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = interval(1000).pipe(
+      map((value) => value * 2)
+    ).subscribe({
+      next(value) {
+        console.log(value);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe);
+  }
 
 }
